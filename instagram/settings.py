4709c6141c2fc8c3a2233718550a301c301be4ba
Mahdi90g <mahdi.ghelich.k@gmail.com>
2024-09-21 01:10:00 +0330

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nzz_8##rwqb(=%^yjf6-z+(bkm@(114j0#bor4^#g-vi-n(4sp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['instamahroom.pythonanywhere.com']
 
 
 # Application definition
